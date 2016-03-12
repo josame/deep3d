df40b45a79309f10d8d65dc89185aa57fc28aab5
Ubuntu <ubuntu@ip-172-31-8-211.us-west-1.compute.internal>
2016-03-12 15:26:21 +0000

    Updated drop15 and drop35 models using correct dropout script

--- alex_best_35/test_my_caffe_model.sh
+++ alex_best_35/test_my_caffe_model.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-~/caffe/build/tools/caffe test -model /mnt/remote_ubuntu/deep3d/alex_best_35/alex_train_v11.prototxt -weights /mnt/remote_ubuntu/deep3d/alex_best_35/alex_best_net_snapshot_iter_15000.caffemodel -gpu 0 -iterations 300
+~/caffe/build/tools/caffe test -model /mnt/remote_ubuntu/deep3d/alex_best_35/alex_train_v11.prototxt -weights /mnt/remote_ubuntu/deep3d/alex_best_35/alex_best_net_snapshot_iter_60000.caffemodel -gpu 0 -iterations 300
--- tester/test_my_caffe_model.sh
+++ tester/test_my_caffe_model.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-~/caffe/build/tools/caffe test -model /mnt/remote_ubuntu/deep3d/alex3d/alex_train.prototxt -weights /mnt/remote_ubuntu/deep3d/alex3d/alex3d_net_snapshot_iter_400000.caffemodel -gpu 0 -iterations 300
+~/caffe/build/tools/caffe test -model /mnt/remote_ubuntu/deep3d/alex3d_v11/alex_train.prototxt -weights /mnt/remote_ubuntu/deep3d/alex3d/alex3d_net_snapshot_iter_400000.caffemodel -gpu 0 -iterations 300
